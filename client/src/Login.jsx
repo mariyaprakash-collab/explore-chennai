a090c236eae691aee132dca86f9e9cc8d5880726
mariyaprakash-collab <mariyaprakash710@gmail.com>
2026-08-17 21:23:59 +0530

    added login frontend for admin

---
+++ client/src/Login.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./login.css"
+
+function Login(){
+    const [name,setName]=useState("");
+    const [password,setPass]=useState("");
+    
+    return(
+        <div>
+            <div className="loginpage">
+                <div className="loginform">
+                    <div className="loginhead">
+                        <h2>Login as admin</h2>
+                    </div>
+                    
+                    <form> 
+                            <input 
+                                type="text"
+                                value={name}
+                                onChange={(e)=>setName(e.target.value)}
+                                placeholder="Enter name"
+                            />
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e)=>setPass(e.target.value)}
+                                placeholder="Enter password"
+                            />
+                            <input
+                                className="submit"
+                                type="submit"
+                            />
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+};
+export default Login;
