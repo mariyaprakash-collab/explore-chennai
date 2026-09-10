fc15c2e81f41f4e7b3b1da79d0d3fc5bcc4cdd52
mariyaprakash-collab <mariyaprakash710@gmail.com>
2026-09-10 18:40:31 +0530

    adding some console.log in logincontroller to debug the login error

--- backend/controllers/logincontroller.js
+++ backend/controllers/logincontroller.js
@@ -1,13 +1,23 @@
 const admindetail = require("../models/admindetail");
 
 const loginadmin = async(req,res) =>{
+    try{
         const {email,password}=req.body;
+
+        console.log("email received:",email);
+        console.log("password received:",password);
+
         const account=await admindetail.findOne({email})
+
+        console.log("account found:",account);
+
         if (!account){
             return res.status(401).json({
                 message: "Invalid email or password"
             })
         }
+
+        console.log("db password:", account.password)
         if (password === account.password) {
             return res.status(200).json({
                 message:"Logged in successfully"
@@ -18,6 +28,13 @@ const loginadmin = async(req,res) =>{
                 message: "Invalid email or password"
             })
         }
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message: "something went wrong",
+            error:error.message
+        });
+    }
 }
 
 module.exports={
